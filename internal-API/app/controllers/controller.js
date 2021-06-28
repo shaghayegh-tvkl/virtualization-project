138f0912e94d7c2eb9d69c6a52334bd4864a8edd
Shaghayegh Tavakoli <shaghayeghtavakoli2@gmail.com>
2021-06-28 12:47:34 +0430

    removed controller unused modules

--- internal-API/app/controllers/controller.js
+++ internal-API/app/controllers/controller.js
@@ -1,7 +1,3 @@
-const md5 = require("md5");
-const uuidv1 = require("uuid/v1");
-const uuidv4 = require("uuid-random");
-
 module.exports = class controller {
   constructor() {
     this.Sequelize = require(`${config.path.models}/index`).Sequelize;
